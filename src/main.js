17b19554fa6ee26d0768186253d07bee0041d76c
Donald Lee <dlqs@gmx.com>
2021-04-01 11:44:26 +0800

    comments

--- src/main.js
+++ src/main.js
@@ -2,10 +2,36 @@ const runLLC = require("./run_llc.js");
 const runLLD = require("./run_lld.js");
 const runWasm = require("./run_wasm.js");
 
+/**
+ * Compiles LLVM IR to an object file with llvm/llc.
+ * 
+ * @param {string} ir - LLVM IR
+ * @return {Promise<string>} obj - Compiled object file in hex
+ */
 const compile = runLLC.runLLC;
+
+/**
+ * Links an object file against libc, libc++ etc. with llvm/lld.
+ * 
+ * @param {string} obj - Object file in hex
+ * @return {Promise<string>} wasm - Runnable WASM binary in hex
+ */
 const link = runLLD.runLLD;
+
+/**
+ * Runs a WASM binary.
+ * 
+ * @param {string} wasm - Runnable WASM binary in hex
+ * @return {Promise<string>} stdout - output
+ */
 const run = runWasm.runWasm;
 
+/**
+ * Compiles, links and runs LLVM IR.
+ *
+ * @param {string} ir - LLVM IR
+ * @return {Promise<string>} stdout - output
+ */
 function compileLinkRun(code) {
   return compile(code)
     .then((obj) => link(obj))
